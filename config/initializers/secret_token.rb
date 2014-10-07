f652f6595c5c9fd030e598fb20d36963fa747792
“Jason <j.lukasson@gmail.com>
2014-10-07 11:01:54 -0500

    created g-tracker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoalTracker::Application.config.secret_token = 'e4f39580c498a26f8e690ff34b5dbd6f3d9006281c19afda60cf57033d2d60003b098a61d57a28e4baa90a1b12e2ee21bbaa37d2613f6dde9e0c562ed78e9570'
