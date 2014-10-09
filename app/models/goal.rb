6bd7cb5aabc8f675b32c916b5c6b804daedb4461
“Jason <j.lukasson@gmail.com>
2014-10-09 10:09:35 -0500

    added sum_of_moments method to goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,11 @@ class Goal < ActiveRecord::Base
   belongs_to :user
   has_many :moments
   
+  def sum_of_moments
+    moments_records=self.moments
+    moments_records.inject{|sum,m| sum + m.moment_value }
+  end
+  
 end
 
 
