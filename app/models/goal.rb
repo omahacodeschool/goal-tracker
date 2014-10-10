70333786dbc3e547b35f66e3caa847279cedcce7
“Jason <j.lukasson@gmail.com>
2014-10-10 11:06:08 -0500

    added reminder method to tracker_mailer.rb and updated Goal.check_for_laziness() in goal.rb

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,22 +3,45 @@ class Goal < ActiveRecord::Base
   belongs_to :user
   has_many :moments
 
-  def check_time
+  # Internal: Determine the difference in time between Time.now and when last moment was created
+  #
+  # self.moments - the Array of Moments belonging to a particular Goal
+  #
+  # Example:
+  #
+  # goal_object.seconds_since_last_moment_created
+  # => 145003
+  #
+  # Returns the seconds since a moment was created for a goal
+  def seconds_since_last_moment_created
     Time.now - self.moments.last.created_at    
   end
   
-  def self.check_for_laziness
+  # Internal: Determines whether user needs an email notification of goal inactivity and sends email if too much time has lapsed; checks every Goal for a User
+  #
+  # time_in_seconds - the maximum amount of seconds to pass since creation of Moment for a Goal
+  #
+  # Example:
+  #
+  # Goal.check_for_laziness
+  # => [#<Goal id: 2, title: "goal 2", goal_value: 500, user_id: nil, completed: nil, created_at: "2014-10-08 17:20:33", updated_at: "2014-10-08 17:20:33">,
+ #<Goal id: 1, title: "goal 1", goal_value: 25, user_id: 1, completed: nil, created_at: "2014-10-08 17:01:45", updated_at: "2014-10-08 20:59:19">]
+ #
+ # Returns an Array of a table Goal objects that will receive email reminders
+  def self.check_for_laziness(time_in_seconds)
     goals = Goal.all
+    email_list=[]
     goals.each do |g|
       user = User.find_by_id(g.user_id)
       if g.moments.length >= 1
-        if g.check_time > 300
-          TrackerMailer.delay.welcome(user)
+        if g.seconds_since_last_moment_created > time_in_seconds
+          TrackerMailer.delay.reminder(user)
+          email_list << g
         end
-      else
-        puts "no mail"
       end
     end
+    return email_list
   end
+  
 end
 
